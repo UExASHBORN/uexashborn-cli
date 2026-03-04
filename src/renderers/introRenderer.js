0ef9a2329b8ad9b32e0bb440e9b65a8f3df7c37f
Adeel Arif <adeelarifknp@gmail.com>
2026-03-05 01:04:55 +0530

    stable core engine + svg assets for intro

---
+++ src/renderers/introRenderer.js
@@ -0,0 +1,20 @@
+export function renderIntro(container) {
+
+    container.innerHTML = `
+        <div class="intro-container">
+
+            <div class="intro-text">
+                searching for adeel...
+            </div>
+
+            <div class="ashborn-wrapper">
+                <img src="/src/assets/closeup-rifle-less-px.svg" class="ashborn-svg">
+            </div>
+
+            <div class="intro-quote">
+                You have found Ashborn.
+            </div>
+
+        </div>
+    `;
+}
